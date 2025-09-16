300360ed14a6c7567d00a8c22de42a30162307ee
DEVitor <costasilvavitorhugo080@gmail.com>
2025-09-15 21:32:11 -0300

    aula 14

---
+++ aula13/node_d578595840c6b.tmp.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c <= 1000000000) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
