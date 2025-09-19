98cab59b59c95305acc705ac1dfe57bd59bf62e5
DEVitor <costasilvavitorhugo080@gmail.com>
2025-09-19 18:09:42 -0300

    ex 17 incompleto

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,11 +1,32 @@
 function contar() {
-    var ini = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var res = document.getElementById('res')
+    let ini = document.getElementById('inicio')
+    let end = document.getElementById('fim')
+    let pass = document.getElementById('passo')
+    let res = document.getElementById('res')
 
-    for (var c = ini.value; c <= fim.value; c += passo.value) {
-        res.length.value.innerHTML = 'Contando:'
-        res.length.value.innerHTML += ` ${c} \u{1F449}`
+    if (ini.value.length == 0 || end.value.length == 0 || pass.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(end.value)
+        let p = Number(pass.value)
+        if (p == 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+        
     }
+    
 }
---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,10 @@
+function gerar() {
+    let num = document.getElementById('inum')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+    }
+
+}
